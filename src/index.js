62d0a1afd5241766d46c82131941f0492f567d13
Alanch8 <aitor.lancharro@gmail.com>
2019-03-04 23:02:12 +0100

    First Commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const methodOverride = require('method-override');
+const session = require('express-session');
+
+
+// Initializations
+const app = express();
+require('./database');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Global Variables
+
+// routes
+app.use(require('./routes'));
+app.use(require('./routes/users'));
+app.use(require('./routes/notes'));
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Server is listening
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
+});
