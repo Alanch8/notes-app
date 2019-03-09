d6d0130e2fe84efe610033fd7a0f2c9354094a4e
alanch8 <aitor.lancharro@gmail.com>
2019-03-09 23:04:07 +0100

    Final

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const path = require("path");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require('passport');
 
 // Initializations
 const app = express();
-require("./database");
+require('./database');
+require('./config/passport');
 
-// settings
+// Settings
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.engine(
@@ -23,7 +25,7 @@ app.engine(
 );
 app.set("view engine", ".hbs");
 
-// middlewares
+// Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 app.use(
@@ -33,22 +35,26 @@ app.use(
     saveUninitialized: true
   })
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 
 // Global Variables
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash("success_msg");
-  res.locals.error_msg = req.flash("error_msg");
-
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
-// routes
+// Routes
 app.use(require("./routes"));
 app.use(require("./routes/users"));
 app.use(require("./routes/notes"));
 
-// static files
+// Static files
 app.use(express.static(path.join(__dirname, "public")));
 
 // Server is listening
